74166e39fab75709257e92d2afb83743036a6b88
Isaac Dozier <isaactdozier@gmail.com>
2018-01-13 07:51:24 +0000

    v.1.0.2-alpha: fix bug causing miscalculations with negative numbers. added error messages to history.

--- calculate.js
+++ calculate.js
@@ -1,6 +1,7 @@
 var userInput;
+var temp, tempActive = false;
 var inchTrue, footTrue, sixTrue, sixDeci, sixRoun, sixRema;
-var rem, p = '';
+var rem, p = '', n = '';
 var foot = 0, inch = 0, six = 0;
 var fis, result, output; 
 var dec = 0, rem;
@@ -8,42 +9,74 @@ var history;
 
 function calculate(num) {
     if(event.keyCode == 13) {
-       footTrue = num.value;
-       inchTrue = num.value * 12;
-       sixTrue  = inchTrue * 16;
-       sixRoun = Math.round(sixTrue);
-	   
-	   foot = Math.floor((sixRoun / 16) / 12);
-       inch = Math.floor(sixRoun / 16) - (foot * 12);
-       six  = sixRoun - (((foot * 12) + inch) * 16);
-       dec  = sixTrue - sixRoun;
-	   
-	   fis = foot + " - " + inch + " - " + six;
-	   rem = dec.toFixed(2);
-	   userInput = num.value + "'";
-	   
-	   p = '';
-	   if(rem > 0){
-           p = "+";
-       }
-	   
-       output = "<div class='output'><span class='fis'>" 
-                        + userInput + " = " + fis 
-                    + "</span>"
-                    + "<span class='decimal rem-" 
-                    + Math.sign(dec.toFixed(2)) + "'>"
-                    +  p  + rem + " / 16"
-                    + "</span>" 
-              + "</div>";
-       
-       if(!history){
-       	result = output;
-       }else {
-       	result = output + document.getElementById('result').innerHTML;
-       }
-       
-       document.getElementById('result').innerHTML = result;
-       
-       document.getElementById('f').value = '';
+        if(!isNaN(num.value)){
+            temp = num.value;
+            tempActive = false;
+            if(num.value < 0){
+                temp = num.value * -1;
+                tempActive = true;
+            }
+            footTrue = temp;
+            inchTrue = temp * 12;
+            sixTrue  = inchTrue * 16;
+            sixRoun = Math.round(sixTrue);
+    	   
+    	    foot = Math.floor((sixRoun / 16) / 12);
+            inch = Math.floor(sixRoun / 16) - (foot * 12);
+            six  = sixRoun - (((foot * 12) + inch) * 16);
+            dec  = sixTrue - sixRoun;
+    	   
+    	    fis = foot + " - " + inch + " - " + six;
+    	    rem = dec.toFixed(2);
+    	    userInput = num.value + "'";
+    	   
+    	    p = '';
+    	    if(rem > 0){
+               p = "+";
+            }
+            
+            n= '';
+            if(tempActive === true){
+                n = "-";
+            }
+    	   
+            output = "<div class='output'><span class='fis'>" 
+                         + userInput + " = " + n + fis 
+                        + "</span>"
+                        + "<span class='decimal rem-" 
+                        + Math.sign(dec.toFixed(2)) + "'>"
+                        +  p  + rem + " / 16"
+                        + "</span>" 
+                   + "</div>";
+            
+            //compile history
+            historyCheck();
+            document.getElementById('result').innerHTML = result;
+            
+            //clear input box
+            document.getElementById('f').value = '';
+        } else {
+            
+            output = "<div class='output error'>" + 
+                        errorMsg
+                   + "</div>";
+            
+            //add error message to history
+            historyCheck();
+            document.getElementById('result').innerHTML = result;
+            
+            //clear input box
+            document.getElementById('f').value = '';
+        }
+    }
+}
+
+var errorMsg = "error";
+
+function historyCheck(){
+    if(!history){
+    	result = output;
+    }else {
+   	    result = output + document.getElementById('result').innerHTML;
     }
 }
