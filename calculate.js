88cb1c9d2aa9c83e38ea7e0123fe413cf5605449
Isaac Dozier <isaactdozier@gmail.com>
2018-01-21 17:16:12 +0000

    Added feature allowing subtraction to be used in expressions. Expressions can currently include addition and subtraction operators simultaniously. Negative numbers can also be used in expressions. Fixed big causing negitive numbers to miscalculate. Fixed bug causing zero to equate as negative. Fixed error handling.

--- calculate.js
+++ calculate.js
@@ -1,160 +1,180 @@
-var userInput = '';
-var tempActive = false;
-var inchTrue, footTrue, sixTrue, sixRoun;
-var rem, p = '', n = '';
-var foot = 0, inch = 0, six = 0;
-var fis, result, outputOne, outputTwo, compile; 
-var dec = 0;
-var history;
+var userInput = ''
+var tempActive = false
+var inchTrue, footTrue, sixTrue, sixRoun
+var rem, p = '', n = ''
+var foot = 0, inch = 0, six = 0
+var fis, result, outputOne, outputTwo, compile 
+var dec = 0
+var history
 
 function calculate() {
-    var temp = document.getElementById('f');
+    var temp = document.getElementById('f')
     if(event.keyCode == 13 || event.button == 0) {
-        if(temp.value === ''){
-            printAlert();
+        if(temp.value === '' || temp.value === 0){
+            printAlert()
         } 
         else
         if(isNumber(temp.value)){
-             printOutput(temp.value,convertDecToFis(temp.value));
+             printOutput(temp.value,convertDecToFis(temp.value))
         } 
         else 
         if(isFis(temp.value)){
-            printOutput(temp.value,convertFisToDec(temp.value));
+            printOutput(temp.value,convertFisToDec(temp.value))
         } 
         else 
         if(isExpression(temp.value)){
-            printOutput(temp.value,convertDecToFis(doMath(temp.value)));
+            printOutput(temp.value,convertDecToFis(doMath(temp.value)))
         } 
         else {
-            printError(temp.value);
+            printError(temp.value)
         }
     }
 }
 
 function doMath(exp){
-    var list = exp.split("+");
+    var list = exp.split("+")
     
     // add empty index 0 to store result
-    list.splice(0,0,0);
+    list.splice(0,0,0)
+    
+    console.log('math: list: ')
+    console.log(list)
     
     //loop through expression, do some math
     while(list.length > 1){
-        var temp = list[1];
-        if(isFis(list[1])){
-            temp = convertFisToDec(list[1])[0]
+        var temp = list[1]
+        var tempArray = temp.split('-')
+        if(isFis(temp)){
+            temp = convertFisToDec(temp)[0]
         } 
-        list[0] = Number(list[0]) + Number(temp);
-        list.splice(1,1);
+        if(tempArray.length > 1){
+            while(tempArray.length > 1){
+                if(isFis(tempArray[0])){tempArray[0] = convertFisToDec(tempArray[0])[0]}
+                if(isFis(tempArray[1])){tempArray[1] = convertFisToDec(tempArray[1])[0]}
+                temp = Number(tempArray[0]) - Number(tempArray[1])
+                tempArray.splice(1,1)
+            }
+        }
+        
+        console.log(temp)
+        
+        list[0] = Number(list[0]) + Number(temp)
+        list.splice(1,1)
     }
     
+    // return Decimal and
     // convert expression result to FIS standard
-    return Number(list[0]);
+    return Number(list[0])
 }
 
 function convertDecToFis(decimal){
-    var temp = decimal;
+    var temp = decimal
     
-    tempActive = false;
-    if(decimal < 0){
-        temp = decimal * -1;
-        tempActive = true;
+    tempActive = false
+    if(returnNegative(decimal)){
+        temp = decimal * -1
+        tempActive = true
     }
     
-    footTrue = temp;
-    inchTrue = temp * 12;
-    sixTrue  = inchTrue * 16;
-    sixRoun = Math.round(sixTrue);
+    footTrue = temp
+    inchTrue = temp * 12
+    sixTrue  = inchTrue * 16
+    sixRoun = Math.round(sixTrue)
    
-    foot = Math.floor((sixRoun / 16) / 12);
-    inch = Math.floor(sixRoun / 16) - (foot * 12);
-    six  = sixRoun - (((foot * 12) + inch) * 16);
-    dec  = sixTrue - sixRoun;
+    foot = Math.floor((sixRoun / 16) / 12)
+    inch = Math.floor(sixRoun / 16) - (foot * 12)
+    six  = sixRoun - (((foot * 12) + inch) * 16)
+    dec  = sixTrue - sixRoun
    
-    fis = foot + " - " + inch + " - " + six;
-    rem = (dec*100).toFixed(2);
+    fis = foot + " - " + inch + " - " + six
+    rem = (dec*100).toFixed(2)
+    console.log('dec-fis: value: ')
+    console.log([fis,rem])
     
     return [fis,rem]
 }
 
 function convertFisToDec(fis){
-    var temp = fis.split('-');
+    var temp = fis.split('.')
     
-    tempActive = false;
+    tempActive = false
     if(temp[0] === '' && temp.length > 3){
-        tempActive = true;
-        temp.splice(0,1);
+        tempActive = true
+        temp.splice(0,1)
+        console.log('fis-dec: is-negative: ')
+        console.log(temp)
     }
     
-    temp[1] = temp[1] / 12;
-    temp[2] = (temp[2] / 16) / 12;
-    console.log (temp);
-    dec = (Number(temp[0]) + temp[1] + temp[2]).toFixed(3);
+    temp[1] = temp[1] / 12
+    temp[2] = (temp[2] / 16) / 12
+    console.log (temp)
+    dec = (Number(temp[0]) + temp[1] + temp[2]).toFixed(3)
    
-    return [dec];
+    return [dec]
 }
 
 
 
 function printOutput(a,b){
-    var remainder = (b[1]/100).toFixed(2);
+    var remainder = (b[1]/100).toFixed(2)
     
-    n = '';
+    n = ''
     if(tempActive){
-        remainder = remainder * -1;
-        n = "-";
+        remainder = remainder * -1
+        n = "-"
     }
     
-    p = '';
+    p = ''
     if(remainder > 0){
-        p = "+";
+        p = "+"
     }
     
-    outputOne = "<span class='fis'>" 
+    outputOne = "<span class='solution'>" 
                     + a + " = " + n + b[0] 
-                + "</span>";
+                + "</span>"
                 
     if(b[1]){
         outputTwo = "<span class='decimal rem-" 
                         + Math.sign(b[1]) + "'>"
                         + p + remainder
-                    + "</span>";
+                    + "</span>"
     } else{
-        outputTwo = '';
+        outputTwo = ''
     }
     
-    compile = "<div class='output'>" + outputOne + outputTwo + "</div>";
+    compile = "<div class='output'>" + outputOne + outputTwo + "</div>"
     
     //compile history
-    document.getElementById('result').innerHTML = historyCheck(compile);
+    document.getElementById('result').innerHTML = historyCheck(compile)
     
     //clear input box & user message
-    document.getElementById('f').value = '';
-    document.getElementById('userMessage').innerHTML = '';
+    document.getElementById('f').value = ''
+    document.getElementById('userMessage').innerHTML = ''
 }
 
 function printError(source){
-    var errorMsg = "error:" + source + " is not a number or expression";
-    outputOne = "<div class='output error'>" + errorMsg + "</div>";
+    var errorMsg = "error:" + source + " is not a number or expression"
+    outputOne = "<div class='output error'>" + errorMsg + "</div>"
     
     //compile history
-    document.getElementById('result').innerHTML = historyCheck(outputOne);
-    document.getElementById('userMessage').innerHTML = 'enter a number';
+    document.getElementById('result').innerHTML = historyCheck(outputOne)
+    document.getElementById('userMessage').innerHTML = 'enter a number'
     
     //clear input box & user message
-    document.getElementById('f').value = '';
-    document.getElementById('userMessage').innerHTML = '';
+    document.getElementById('f').value = ''
+    document.getElementById('userMessage').innerHTML = ''
 }
 
 function printAlert(){
-    document.getElementById('userMessage').innerHTML = 'enter a number';
+    document.getElementById('userMessage').innerHTML = 'enter a number'
 }
 
 function historyCheck(h){
-    if(!history){
-    	return h;
-    }else {
-   	    return h + document.getElementById('result').innerHTML;
-    }
+    if(!history)
+    	return h
+    else 
+   	    return h + document.getElementById('result').innerHTML
+    
 }
 
 function isNumber(x){
@@ -162,21 +182,35 @@ function isNumber(x){
 }
 
 function isFis(z){
-    var zArray = z.split('-');
-    var length = zArray.length;
+    var zArray = z.split('.')
     
-    if(zArray[0] === ''){
-        z.splice(0,1);
+    tempActive = false
+    if(zArray[0] === '' && zArray.length > 1){
+        zArray.splice(0,1)
+        tempActive = true
     }
     
-    var len   = length === 3;
-    var isNumOne = isNumber(zArray[0]);
-    var isNumTwo = isNumber(zArray[length-1])
+    var len   = zArray.length === 3
+    var isNumOne = isNumber(zArray[0])
+    var isNumTwo = isNumber(zArray[zArray.length-1])
+    
+    console.log(zArray)
+    console.log(len)
+    console.log(isNumOne)
+    console.log(isNumTwo)
     
     return len && isNumOne && isNumTwo
     
 }
 
 function isExpression(y){
-    return y.split('+').length > 1
+    return y.split(/([-+])\w+/g).length > 1
+}
+
+function returnNegative(n){
+    return 0 > n
+}
+
+function reverseZero(np) {
+    return np * -1
 }
