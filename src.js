343bdccb6fc8681228a384b10775c62c51e92e86
Isaac Dozier <isaactdozier@gmail.com>
2020-04-10 17:36:38 -0700

    v1.0.0 load

--- src.js
+++ src.js
@@ -1,5 +1,7 @@
 const b = "<br/>"
 
+var lat, lng
+
 var request = new XMLHttpRequest();
 
 request.open('GET', 'https://api.ipdata.co/?api-key=test');
@@ -10,22 +12,38 @@ request.onreadystatechange = function () {
   if (this.readyState === 4) {
   	var data = JSON.parse(this.responseText);
 
-    document.getElementById('ip').innerHTML = data.ip + b
+    document.getElementById('ip').innerHTML = "<b>IP Address:</b>" + b
+    										+ data.ip + b
     										+ b
+    										+ "<b>Physical Location:</b>" + b
     										+ data.city +", "+ data.region_code + b
     										+ data.continent_name + b
     										+ b
+    										+ "<b>GPS Coordinates:</b>" + b
     										+ data.latitude +", "+ data.longitude + b
     										+ b
+    										+ "<b>ISP Provider:</b>" + b
     										+ data.asn.name + b
     										+ b
+    										+ "<b>Timezone:</b>" + b
     										+ data.time_zone.abbr + " - " + data.time_zone.name + b
     										+ data.time_zone.current_time + b
     										+ b;
 
-    console.log(data)
+    lat = Number(data.latitude)
+    lng = Number(data.longitude)
 
+    initMap()
   }
 };
 
 request.send();
+
+var map;
+
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: lat, lng: lng},
+  zoom: 8
+});
+}
